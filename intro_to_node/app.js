6dccbc9b7a8e7539e3311ba836955212fd790e08
Zee Celest <zeecelest@gmail.com>
2019-10-28 20:33:22 -0400

    finished 2nd drill

--- intro_to_node/app.js
+++ intro_to_node/app.js
@@ -11,12 +11,27 @@ app.get('/queryViewer', (req, res) => {
   res.json(req.query); //do not send any data back to the client
 });
 
+//drill 1 route handler for /sum
 app.get('/sum', (req, res) => {
   const a = req.query.a;
   const b = req.query.b;
   const c = parseInt(a) + parseInt(b);
   const result = `The sum of ${a} and ${b} is ${c}.`;
 
+  res.send(result);
+});
+
+//creating cipher endpoint drill
+app.get('/cipher', (req, res) => {
+  const text = req.query.text;
+  const shift = parseInt(req.query.shift);
+  let result = '';
+  for (i = 0; i < text.length; i++) {
+    const charCodeAt = text.charCodeAt(i) + shift;
+    const char = String.fromCharCode(charCodeAt);
+    result = result + char;
+
+  }
   res.send(result);
 });
 
